5c58a35425534b118606f6afcbdd624e195ae4ce
DanielKRing1 <DanielKRing1@gmail.com>
2019-08-13 18:08:48 -0700

    Initialize firebase app

--- Firebase/init.js
+++ Firebase/init.js
@@ -1,9 +1,8 @@
 const admin = require("firebase-admin");
 
 module.exports = () => {
-    
-
     // Initialize the default app
     var admin = require('firebase-admin');
+    console.log('INIT FIREBASE APP');
     var app = admin.initializeApp();
 }
