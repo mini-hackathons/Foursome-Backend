0174a8332edb3b15bda12358aa5b14bd01fe4de8
DanielKRing1 <DanielKRing1@gmail.com>
2019-08-13 19:48:42 -0700

    Initialize firebase app

--- Firebase/init.js
+++ Firebase/init.js
@@ -2,6 +2,7 @@ const admin = require("firebase-admin");
 
 module.exports = () => {
     admin.initializeApp({
-        credential: admin.credential.applicationDefault()
-    });
+        credential: admin.credential.applicationDefault(),
+        databaseURL: 'https://foursome-ebe6a.firebaseio.com'
+      });
 }
